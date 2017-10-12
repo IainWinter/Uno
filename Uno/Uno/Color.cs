3b85782c935d3ba18a3f7c4ab04d244ee422b8cd
HappiGF <lggufi@gmail.com>
2017-10-12 13:35:14 -0400

    OMG PLEASE WORK

---
+++ Uno/Uno/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno {
+    enum Color {
+        Blue,
+        Red,
+        Green,
+        Yellow,
+        Wild
+    };
+}
---
+++ Uno/Uno/Hand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno {
+    class Hand<T>:Pile<T> {
+      
+        public void DealToHand(Card card) {
+            Hand<T>.Add(card);
+        }
+    }
+}
