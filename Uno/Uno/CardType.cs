03705b7a0fa8247fafb79e8b445512c6d2fcb06d
HappiGF <lggufi@gmail.com>
2017-10-12 12:58:56 -0400

    Implements Card, CardType, Color, and Hand

---
+++ Uno/Uno/CardType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno {
+    enum CardType {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven, 
+        Eight,
+        Nine,
+        Reverse,
+        Skip,
+        DrawTwo,
+        DrawFour,
+        Wild,
+    };
+}
