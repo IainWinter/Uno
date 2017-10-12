744925987aefacc017dc67cddae7a8ecebc1480b
HappiGF <lggufi@gmail.com>
2017-10-12 13:30:56 -0400

    PLEASE WORK

--- Uno/Uno/Hand.cs
+++ Uno/Uno/Hand.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Uno {
     class Hand<T>:Pile<T> {
-
+      
+        public void DealToHand(Card card) {
+            Hand<T>.Add(card);
+        }
     }
 }
