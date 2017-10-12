cbdfd7e57b406d7484ac82c809fcddfc776694ce
Iain Winter <iainwinter1@gmail.com>
2017-10-12 12:32:23 -0400

    Added Pile.cs

---
+++ Uno/Uno/Pile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno {
+    public class Pile<T> {
+        private List<T> pile;
+
+        public Pile() : this(0) { }
+        public Pile(int capacity) : this(new List<T>(capacity)) { }
+        public Pile(List<T> collection) {
+            pile = collection;
+        }
+    }
+}
