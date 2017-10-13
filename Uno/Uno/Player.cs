73835c7c43fc30420a256b0c9f21f7379e10d129
jacob <cuby474@gmail.com>
2017-10-13 12:48:31 -0400

    the real player

--- Uno/Uno/Player.cs
+++ Uno/Uno/Player.cs
@@ -1,22 +1,37 @@
 ﻿using System;
-namespace Uno {
-    class Player {
+namespace Uno
+{
+    class Player
+    {
         private String name;
-        private Hand<Card> _hand;
-
-        public Hand<Card> Hand => _hand;
-
-        public Player(String playerName) {
+        private Hand<Card> pHand;
+        private Deck<Card> deck;
+        public Player(String playerName)
+        {
             name = playerName;
         }
 
-        public void DealToHand(Card c) {
-            _hand.DealToHand(c);
+        public Hand<Card> getpHand()
+        {
+            return pHand;
         }
 
-        public Card DoTurn() {
+        public void setpHand(Hand<Card> newHand)
+        {
+            pHand = newHand;
+        }
+
+        public void DealToHand(Card c)
+        {
+            pHand.DealToHand(c);
+        }
+
+        public Card DoTurn()
+        {
             Console.WriteLine("It is now " + name + "'s turn");
-            Console.Write(_hand.ToString());
+            Console.Write(pHand.ToString());
+            Console.WriteLine("play a card!");
+            int played = int.Parse(Console.ReadLine());
 
             return;
         }
