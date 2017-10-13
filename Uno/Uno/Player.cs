9823b27cf1eab1bfc82b5045712df6e58854698f
jacob <cuby474@gmail.com>
2017-10-13 13:41:58 -0400

    now more robust DoTurn()

--- Uno/Uno/Player.cs
+++ Uno/Uno/Player.cs
@@ -15,15 +15,25 @@ namespace Uno {
             _hand.DealToHand(c);
         }
 
-        public Card DoTurn()
+        public Card DoTurn(Card c)
         {
             Console.WriteLine($"It is now {name}'s turn");
             Console.Write(_hand.ToString());
+            Console.Write("The top card is " + c.ToString());
             Console.WriteLine("play a card!");
 
             int played = int.Parse(Console.ReadLine());
-            return _hand.PlayCard(played);
+            Card playedCard = _hand.PlayCard(played);
+            while (true){
+                if (playedCard.GetType() == c.GetType() || playedCard.GetColor() == c.GetColor() || playedCard.GetType() == CardType.Wild){
+                    return playedCard;
+                }
+                else{
+                    Console.WriteLine("Invalid play, try again");
+                }
+            }
         }
+
         public Boolean HasWon(){
             return _hand.GetSize() == 0;
         }
