ee7eed704571946beec658fcda615dbeda9ddc19
HappiGF <lggufi@gmail.com>
2017-10-13 12:21:06 -0400

    Hand without Pile

--- Uno/Uno/Hand.cs
+++ Uno/Uno/Hand.cs
@@ -5,14 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Uno {
-    class Hand<T> : Pile<T> {
-        
-        public void DealToHand(T card) {
-            Add(card);
+    class Hand<Card> {
+
+        private List<Card> _hand;
+
+        public void DealToHand(Card card) {
+            _hand.Add(card);
         }
 
-        public T PlayCard(T card) {
-            return Take(card);
+        public void DealToHand(Card[] cards) {
+            _hand.AddRange(cards);
+        }
+
+        public Card PlayCard(Card card) {
+            Card removedCard = _hand[_hand.IndexOf(card)];
+            _hand.Remove(card);
+            return removedCard;
         }
     }
 }
