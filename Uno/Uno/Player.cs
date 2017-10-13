3a42428e0a973696d0134e4bdd7c1e1c6098410e
Iain Winter <iainwinter1@gmail.com>
2017-10-13 13:54:20 -0400

    cleano

--- Uno/Uno/Player.cs
+++ Uno/Uno/Player.cs
@@ -15,11 +15,14 @@ namespace Uno {
             _hand.DealToHand(c);
         }
 
-        public Card DoTurn()
-        {
-            Console.WriteLine($"It is now {name}'s turn");
+        public Card DoTurn() {
+            Console.WriteLine($"It is now {name}'s turn!");
+            Console.WriteLine();
+            Console.WriteLine($"-------- {name}'s Turn --------");
             Console.Write(_hand.ToString());
-            Console.WriteLine("play a card!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Play card: ");
 
             int played = int.Parse(Console.ReadLine());
             return _hand.PlayCard(played);
